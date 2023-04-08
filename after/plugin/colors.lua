22197704e7415c8162e7510e3b88df64bd60a694
paolo-petralia <ptrpla03a05c351d@studium.unict.it>
2023-04-08 18:23:07 +0200

    update

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,10 @@
-
 --function ColorMyPencils(color)
-	--color = color or "onedark"	
-	--vim.cmd.colorscheme(color)
+--color = color or "onedark"	
+--vim.cmd.colorscheme(color)
 --end
 
 --ColorMyPencils()
 
 -- vim.cmd('colorscheme rose-pine')
-require("onedark").load()
+-- require("onedark").load()
+vim.cmd.colorscheme("darkplus")
--- lua/paolo/packer.lua
+++ lua/paolo/packer.lua
@@ -13,11 +13,13 @@ return require('packer').startup(function(use)
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use 'm4xshen/autoclose.nvim'
-    --  use "olimorris/onedarkpro.nvim"
+    -- use "olimorris/onedarkpro.nvim"
     -- use({ 'rose-pine/neovim', as = 'rose-pine' })
     -- Using Packer
-    use 'navarasu/onedark.nvim'
+    -- use 'navarasu/onedark.nvim'
+    -- use({ 'joshdick/onedark.vim', as = 'onedark' })
 
+    use "lunarvim/darkplus.nvim"
     use('p00f/nvim-ts-rainbow')
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('theprimeagen/harpoon')
@@ -35,20 +37,20 @@ return require('packer').startup(function(use)
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },   -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'hrsh7th/cmp-buffer' }, -- Optional
-            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
             { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
             -- Snippets
-            { 'L3MON4D3/LuaSnip' },   -- Required
+            { 'L3MON4D3/LuaSnip' },             -- Required
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
